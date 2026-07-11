dbdb34811c72ac28a4c3c94edadf1600fa2b191f
NNLV7 <nelitsnugi0753@gmail.com>
2026-07-12 01:11:27 +0700

    Use environment variable for socket URL

--- src/socket.ts
+++ src/socket.ts
@@ -1,5 +1,9 @@
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:3001", {
+const serverUrl =
+  import.meta.env.VITE_SOCKET_URL ||
+  "http://localhost:3001";
+
+export const socket = io(serverUrl, {
   autoConnect: false,
 });
